3001023ac27d288aecb07480c9616ba446ae92aa
Sparsh <sparsh.chadha46@gmail.com>
2022-09-26 22:55:11 +0530

    changed again in 1st branch

--- app/src/main/java/com/example/demoprojectforbranchmergetesting/MainActivity.kt
+++ app/src/main/java/com/example/demoprojectforbranchmergetesting/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        print("This message will remain in master branch, NOW ADDED THIS TO firstbranch")
+        print("This message will remain in master branch, NOW ADDED THIS TO firstbranch, now changing somethign in 1st branch")
     }
 }
