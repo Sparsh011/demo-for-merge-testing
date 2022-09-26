96e4828b23d7c25c990ce50b6ee9ced1d93d0a5c
Sparsh <sparsh.chadha46@gmail.com>
2022-09-26 23:04:10 +0530

    commiting in 2nd branch at 11:04

--- app/src/main/java/com/example/demoprojectforbranchmergetesting/MainActivity.kt
+++ app/src/main/java/com/example/demoprojectforbranchmergetesting/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         print("Deleted prev msg and now in second branch")
+        print("Now let's do changes in this one")
     }
 }
